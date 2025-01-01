bbfbd3fe47b3bfba83636160bf22716d291f667e
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-01-01 23:22:54 +0900

    [Merge] develop -> main
    [Feat] path alias 추가 (#16)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@_assets': '/src/assets',
+      '@_components': '/src/components',
+      '@_constants': '/src/constants',
+      '@_hooks': '/src/hooks',
+      '@_layouts': '/src/layouts',
+      '@_mock': '/src/mock',
+      '@_store': '/src/store',
+      '@_styles': '/src/styles',
+      '@_types': '/src/types',
+      '@_utils': '/src/utils',
+    },
+  },
+});

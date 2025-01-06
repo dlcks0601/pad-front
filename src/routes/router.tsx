f5ba8d77a6d1377b98afc1207fd9f85a8a3accfb
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-01-06 11:37:23 +0900

    [Feat] 라우터에 mypage 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,5 +1,9 @@
+import MyPage from '@/components/pages/MyPage';
 import LoginPage from '../components/pages/LoginPage';
 
-const router = [{ path: '/login', element: <LoginPage /> }];
+const router = [
+  { path: '/login', element: <LoginPage /> },
+  { path: '/mypage', element: <MyPage /> },
+];
 
 export default router;

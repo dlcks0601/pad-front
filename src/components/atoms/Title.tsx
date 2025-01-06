70e425327f65d4722bf8442b87f855c233813c9d
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-01-06 21:03:48 +0900

    [Feat] Title 컴포넌트 구현(atoms)

---
+++ src/components/atoms/Title.tsx
@@ -0,0 +1,55 @@
+import { cn } from '@/utils/cn';
+import { cva, VariantProps } from 'class-variance-authority';
+import { HTMLAttributes, PropsWithChildren } from 'react';
+
+const TitleVariants = cva('', {
+  variants: {
+    fontWeight: {
+      medium: 'text-medium',
+      bold: 'text-simebold',
+    },
+    size: {
+      xs: 'text-[14px]',
+      sm: 'text-[20px]',
+      md: 'text-[25px]',
+      lg: 'text-[30px]',
+      xl: 'text-[35px]',
+    },
+    lineClamp: {
+      none: 'line-clamp-none',
+      1: 'text-ellipsis break-all line-clamp-1',
+      2: 'text-ellipsis break-all line-clamp-2',
+    },
+  },
+  defaultVariants: {
+    fontWeight: 'bold',
+    size: 'md',
+    lineClamp: 'none',
+  },
+});
+
+export interface TitleProps
+  extends PropsWithChildren,
+    HTMLAttributes<HTMLHeadingElement>,
+    VariantProps<typeof TitleVariants> {}
+
+const Title = ({
+  children,
+  className,
+  fontWeight,
+  lineClamp,
+  size,
+  ...props
+}: TitleProps) => {
+  console.log(cn(TitleVariants({ lineClamp, fontWeight, size, className })));
+  return (
+    <div
+      {...props}
+      className={cn(TitleVariants({ lineClamp, fontWeight, size, className }))}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Title;

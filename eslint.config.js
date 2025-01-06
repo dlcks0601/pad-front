c70deb0c34d85a7dce4164d7cfefc92d349a744d
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-01-06 09:22:41 +0900

    [Merge] feature/19-ESLint -> develop

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,61 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import airbnbBase from 'eslint-config-airbnb-base';
+import airbnbReact from 'eslint-config-airbnb/rules/react';
+import airbnbHooks from 'eslint-config-airbnb/rules/react-hooks';
+import airbnbTS from 'eslint-config-airbnb-typescript/lib/shared.js';
+import importPlugin from 'eslint-plugin-import';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ['node_modules', 'dist'], // ESLint가 무시할 디렉토리
+  },
+  {
+    files: ['src/**/*.{ts,tsx}'], // TypeScript 및 React 파일에 적용
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: typescriptParser,
+      parserOptions: {
+        project: './tsconfig.app.json',
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true, // React JSX 지원
+        },
+      },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      '@typescript-eslint': typescriptEslintPlugin,
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      'jsx-a11y': jsxA11yPlugin,
+      import: importPlugin,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      ...airbnbBase.rules,
+      ...airbnbReact.rules,
+      ...airbnbHooks.rules,
+      ...airbnbTS.rules,
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-filename-extension': [
+        1,
+        { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
       ],
     },
+    settings: {
+      react: {
+        version: 'detect', // 설치된 React 버전에 따라 자동 감지
+      },
+    },
   },
-)
+  prettierRecommended,
+];
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-const App = () => {
+function App() {
   return <div className='text-blue-600 font-thin'>App</div>;
-};
+}
 
 export default App;

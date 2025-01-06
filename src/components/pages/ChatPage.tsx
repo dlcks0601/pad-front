dc0259c461f530709f0e293aac8352769217a0ee
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-01-06 15:06:58 +0900

    [Feat] ChatPage 컴포넌트(pages)

--- src/components/pages/ChatPage.tsx
+++ src/components/pages/ChatPage.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 
 const ChatPage = () => {
   return (
-    <div className='flex max-w-[1240px]'>
-      <div>
-        <ChatSidebar />
+    <div className='w-full h-full flex justify-center'>
+      <div className='flex max-w-[1240px]'>
+        <div>
+          <ChatSidebar />
+        </div>
+        <div>right</div>
       </div>
-      <div>right</div>
     </div>
   );
 };

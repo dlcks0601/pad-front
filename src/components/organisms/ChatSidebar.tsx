43b8c43323141cccbbf58514abccfe7955ae9f9c
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-01-06 15:06:28 +0900

    [Feat] ChatSidebar 컴포넌트(organisms)

--- src/components/organisms/ChatSidebar.tsx
+++ src/components/organisms/ChatSidebar.tsx
@@ -1,5 +1,31 @@
-import UserListItem from '@/components/molecules/UserListItem';
-import React from 'react';
+import { ListItem } from '@/components/molecules/ListItem';
+import avatar from '@/assets/avatar.png';
+import { cn } from '@/utils/cn';
+import clsx from 'clsx';
+
+const chatRooms = [
+  {
+    id: 1,
+    avatar: avatar,
+    title: '이재혁',
+    subtitle: '이재혁, 이찬, 한태동',
+    lastTime: '1h',
+  },
+  {
+    id: 2,
+    avatar: avatar,
+    title: '이재혁',
+    subtitle: '이재혁, 이찬, 한태동',
+    lastTime: '1h',
+  },
+  {
+    id: 3,
+    avatar: avatar,
+    title: '이재혁',
+    subtitle: '이재혁, 이찬, 한태동',
+    lastTime: '1h',
+  },
+];
 
 const ChatSidebar = () => {
   return (
@@ -8,14 +34,24 @@ const ChatSidebar = () => {
         <input type='text' defaultValue='이름' />
       </div>
       <ul>
-        <li>
-          <UserListItem
-            profileIcon='아이콘'
-            name='이재혁'
-            label1='한태동, 이찬'
-            label2='1h'
-          />
-        </li>
+        {chatRooms.map((room) => (
+          <li key={room.id}>
+            <ListItem>
+              <ListItem.Avatar size='sm' src={room.avatar} />
+              <ListItem.Title className={cn('text-body1')}>
+                {room.title}
+              </ListItem.Title>
+              <ListItem.Subtitle
+                className={clsx('text-caption1', 'text-mediumgray')}
+              >
+                {room.subtitle}
+              </ListItem.Subtitle>
+              <ListItem.Label className={cn('text-caption1')}>
+                {room.lastTime}
+              </ListItem.Label>
+            </ListItem>
+          </li>
+        ))}
       </ul>
     </div>
   );

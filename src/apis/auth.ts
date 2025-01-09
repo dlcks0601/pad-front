d4b75ca1224c577da04f58c739d5d93a727c7b76
HANTAEDONG <htd0913@gmail.com>
2025-01-09 16:21:25 +0900

    [Fix] fetcher 제거

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,3 +1,4 @@
+import { axiosInstance } from '@/apis/@core';
 import { API_PATH } from '@/apis/api-path';
 import { AuthRequest, AuthResponse, TokenResponse } from '@/types/auth.type';
 import { RoleRequest, RoleResponse } from '@/types/role.type';
@@ -25,21 +26,28 @@ export const postAuthorizationCode = async ({
 export const fetchUserRole = async ({
   userRole,
 }: RoleRequest): Promise<RoleResponse> => {
-  const response = await fetcher<RoleResponse>({
-    url: API_PATH.roleSelect,
-    method: 'PUT',
-    data: {
-      role_id: userRole,
-    },
-  });
-  return response.data;
+  try {
+    const response = await axiosInstance.put<RoleResponse>(
+      API_PATH.roleSelect,
+      {
+        role_id: userRole,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error('유저 롤 변경 중 오류 발생:', error);
+    throw error;
+  }
 };
 
 export const updateAccessToken = async (): Promise<TokenResponse> => {
-  const response = await fetcher<TokenResponse>({
-    url: API_PATH.updateToken,
-    method: 'POST',
-  });
-
-  return response.data;
+  try {
+    const response = await axiosInstance.post<TokenResponse>(
+      API_PATH.updateToken
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Access Token 갱신 중 오류 발생:', error);
+    throw error;
+  }
 };

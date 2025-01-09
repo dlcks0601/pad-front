c0d1bb60ef46649f7cdcf1f9094f7ed19300f3f8
HANTAEDONG <132195232+HANTAEDONG@users.noreply.github.com>
2025-01-09 19:36:48 +0900

    Feature/55 refresh token (#57)
    * [Fix] 경로 문제히결
    
    * [Add] console추가
    
    * [Refactor] 백엔드 데이터 형식에 맞춰서 수정
    
    * [Refactor] 백엔드 데이터 형식에 맞춰서 수정2
    
    * [Refactor] withcredentails 추가
    
    * [Fix] fetcher 제거
    
    * [Refactor] env 해제
    
    * [Refactor] baseurl 수정
    
    * [Refactor] baseurl 수정

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,7 +1,7 @@
+import { axiosInstance } from '@/apis/@core';
 import { API_PATH } from '@/apis/api-path';
 import { AuthRequest, AuthResponse, TokenResponse } from '@/types/auth.type';
 import { RoleRequest, RoleResponse } from '@/types/role.type';
-import fetcher from '@/utils/fetcher';
 import axios from 'axios';
 
 export const postAuthorizationCode = async ({
@@ -9,30 +9,44 @@ export const postAuthorizationCode = async ({
   provider,
 }: AuthRequest): Promise<AuthResponse> => {
   const apiPath = API_PATH.login.replace(':provider', provider);
-  const response = await axios.post(apiPath, {
-    code: authorizationCode,
-  });
+  console.log(`api 경로: http://localhost:8080${apiPath}`);
+  const response = await axios.post(
+    `http://43.202.172.0:8080${apiPath}`,
+    {
+      code: authorizationCode,
+    },
+    {
+      withCredentials: true,
+    }
+  );
   return response.data;
 };
 
 export const fetchUserRole = async ({
   userRole,
 }: RoleRequest): Promise<RoleResponse> => {
-  const response = await fetcher<RoleResponse>({
-    url: API_PATH.roleSelect,
-    method: 'PUT',
-    data: {
-      role_id: userRole,
-    },
-  });
-  return response.data;
+  try {
+    const response = await axiosInstance.put<RoleResponse>(
+      API_PATH.roleSelect,
+      {
+        role_id: userRole,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error('유저 롤 변경 중 오류 발생:', error);
+    throw error;
+  }
 };
 
 export const updateAccessToken = async (): Promise<TokenResponse> => {
-  const response = await fetcher<TokenResponse>({
-    url: API_PATH.updateToken,
-    method: 'POST',
-  });
-
-  return response.data;
+  try {
+    const response = await axiosInstance.post<TokenResponse>(
+      API_PATH.updateToken
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Access Token 갱신 중 오류 발생:', error);
+    throw error;
+  }
 };

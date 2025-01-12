8fe3e19216cfa9d6b3c3ad99c666dcdd957d89e4
HANTAEDONG <htd0913@gmail.com>
2025-01-12 22:52:54 +0900

    [Merge] feature/55-refresh-tokend에서 develop으로 병합

--- src/components/organisms/sides/SideMenu.tsx
+++ src/components/organisms/sides/SideMenu.tsx
@@ -2,11 +2,18 @@ import { useState, useEffect, useRef } from 'react';
 import Logo from '@/components/atoms/Logo';
 import Menu from '@/components/molecules/Menu';
 import Avatar from '@/components/atoms/Avatar';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const SideMenu = () => {
   const navigate = useNavigate();
   const [showLogin, setShowLogin] = useState(false);
+  // const [isLoggedIn] = useAuth(useShallow((state) => [state.isLoggedIn]));
+  const isLoggedIn = true;
+  const {
+    isOpen: isSearchModalOpen,
+    openModal: openSearchModal,
+    closeModal: closeSearchModal,
+  } = useModal();
   const loginRef = useRef<HTMLDivElement>(null);
 
   const menuItems: {
@@ -28,7 +35,7 @@ const SideMenu = () => {
     {
       type: 'search',
       label: '검색',
-      onClick: () => alert('네비게이션 연결해주세요'),
+      onClick: () => navigate('/roleselect'),
     },
     {
       type: 'star',
@@ -63,25 +70,27 @@ const SideMenu = () => {
   }, [showLogin]);
 
   return (
-    <div className='flex flex-col justify-between items-center h-full py-[20px]'>
-      <div className='mb-8 cursor-pointer' onClick={() => navigate('/')}>
-        <Logo />
-      </div>
+    <>
+      {isSearchModalOpen && <SearchModal onClose={closeSearchModal} />}
+      <div className='flex flex-col justify-between items-center h-full py-[20px]'>
+        <div className='mb-8 cursor-pointer' onClick={() => navigate('/')}>
+          <Logo />
+        </div>
 
-      <Menu items={menuItems} />
+        <Menu items={menuItems} />
 
-      <div className='relative' ref={loginRef}>
-        <Avatar
-          size='sm'
-          src='/src/assets/Genericavatar.svg'
-          alt='User Avatar'
-          className='cursor-pointer border-4 border-transparent hover:border-gray-300 transition-shadow duration-300'
-          onClick={handleAvatarClick}
-        />
+        <div className='relative' ref={loginRef}>
+          <Avatar
+            size='sm'
+            src='/src/assets/Genericavatar.svg'
+            alt='User Avatar'
+            className='cursor-pointer border-4 border-transparent hover:border-gray-300 transition-shadow duration-300'
+            onClick={handleAvatarClick}
+          />
 
-        {showLogin && (
-          <div
-            className='
+          {showLogin && (
+            <div
+              className='
               absolute
               top-[50%]
               left-full
@@ -92,20 +101,21 @@ const SideMenu = () => {
               transition-opacity
               duration-300
             '
-          >
-            <button
-              className='w-full text-left px-4 py-2 text-[14px] text-gray-800 hover:text-black'
-              onClick={() => {
-                alert('로그인');
-                setShowLogin(false);
-              }}
             >
-              로그인
-            </button>
-          </div>
-        )}
+              <button
+                className='w-full text-left px-4 py-2 text-[14px] text-gray-800 hover:text-black'
+                onClick={() => {
+                  alert('로그인');
+                  setShowLogin(false);
+                }}
+              >
+                로그인
+              </button>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -3,11 +3,11 @@ import DefaultLogo from '@/assets/logos/DefaultLogo.svg';
 
 const LoginPage = () => {
   const handleGoogleLogin = () => {
-    window.location.href = 'http://43.202.172.0:8080/auth/google';
+    window.location.href = `${import.meta.env.VITE_LOCAL_URL}/auth/google`;
   };
 
   const handleGitHubLogin = () => {
-    window.location.href = 'http://43.202.172.0:8080/auth/github';
+    window.location.href = `${import.meta.env.VITE_LOCAL_URL}/auth/github`;
   };
 
   return (

92f165063ae35ed82e02f015dff125fd7f645e00
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-01-17 14:01:22 +0900

    [Feat] 채팅방 제목 검색 기능

--- src/components/organisms/chat/ChatSidebar.tsx
+++ src/components/organisms/chat/ChatSidebar.tsx
@@ -1,29 +1,22 @@
 import SearchInput from '@/components/molecules/chat/SearchInput';
 import ChannelList from '@/components/organisms/chat/ChannelList';
+import useDebounce from '@/hooks/useDebounce';
 import { ChatState, useChatStore } from '@/store/chatStore';
-import { FormEvent, useState } from 'react';
+import { useState } from 'react';
 
 const ChatSidebar = () => {
   const [keyword, setKeyword] = useState('');
-  const [filteredChannels, setFilteredChannels] =
-    useState<ChatState['channels']>();
+  const debouncedKeyword = useDebounce(keyword, 300);
   const channels = useChatStore((state) => state.channels);
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-    const filteredChannels = filterChannels(keyword, channels);
-    setFilteredChannels(filteredChannels);
-  };
+  const filteredChannels = filterChannels(debouncedKeyword, channels);
 
   return (
     <div className='flex flex-col gap-[24px] flex-1'>
-      <form onSubmit={handleSubmit}>
-        <SearchInput
-          value={keyword}
-          onChange={(e) => setKeyword(e.target.value)}
-        />
-        <button>검색</button>
-      </form>
+      <SearchInput
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+      />
       <ChannelList channels={filteredChannels ? filteredChannels : channels} />
     </div>
   );
@@ -31,13 +24,9 @@ const ChatSidebar = () => {
 
 function filterChannels(keyword: string, channels: ChatState['channels']) {
   return Object.fromEntries(
-    Object.entries(channels).filter(([_, channel]) => {
-      const includedTitle = channel.title.includes(keyword);
-      const includedUser = channel.users.some((user) =>
-        user.nickname.includes(keyword)
-      );
-      return includedTitle || includedUser;
-    })
+    Object.entries(channels).filter(([_, channel]) =>
+      channel.title.includes(keyword)
+    )
   );
 }
 

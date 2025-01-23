c2d122d22c340f9c43adede39e12bb5cb0e53ef4
HANTAEDONG <htd0913@gmail.com>
2025-01-23 15:35:20 +0900

    [Feat] 피드 메인 컨텐츠 리치텍스트 적용

--- src/components/atoms/contents/ContentsBody.tsx
+++ src/components/atoms/contents/ContentsBody.tsx
@@ -1,9 +1,22 @@
+import DOMPurify from 'dompurify';
+
 interface ContentsBodyProps {
   body: string;
 }
 
 const ContentsBody = ({ body }: ContentsBodyProps) => {
-  return <div className=' flex text-[14px] font-regular'>{body}</div>;
+  const getTruncatedContent = (html: string) => {
+    const tempDiv = document.createElement('div');
+    tempDiv.innerHTML = DOMPurify.sanitize(html);
+    const textContent = tempDiv.textContent || '';
+    const sentences = textContent.match(/([^.!?]+[.!?]+)/g) || [];
+    const truncatedText =
+      sentences.slice(0, 5).join(' ') + (sentences.length > 5 ? '...' : '');
+    tempDiv.innerHTML = DOMPurify.sanitize(truncatedText);
+    return { __html: tempDiv.innerHTML };
+  };
+
+  return <div dangerouslySetInnerHTML={getTruncatedContent(body)}></div>;
 };
 
 export default ContentsBody;

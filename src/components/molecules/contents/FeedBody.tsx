2764755a9d750b00d1cbb4d3a41c06a00980a682
HANTAEDONG <htd0913@gmail.com>
2025-01-24 14:06:45 +0900

    [Refactor] 피드 리스트 태그 디자인 수정

--- src/components/molecules/contents/FeedBody.tsx
+++ src/components/molecules/contents/FeedBody.tsx
@@ -1,24 +1,29 @@
-// 컨텐츠 타이틀 + 바디 + 태그
 import ContentsTitle from '@/components/atoms/contents/ContentsTitle';
 import ContentsBody from '@/components/atoms/contents/ContentsBody';
-import Tag from '@/components/atoms/Tag';
-import { TagItemKey } from '@/constants/tagItem';
+import { TagItemKey, tagColors } from '@/constants/tagItem';
+import clsx from 'clsx';
 
 interface FeedBodyProps {
   title: string;
   content: string;
   tags: TagItemKey[];
-  sliceBody?: boolean;
 }
 
-const FeedBody = ({ title, content, tags, sliceBody }: FeedBodyProps) => {
+const FeedBody = ({ title, content, tags }: FeedBodyProps) => {
   return (
     <div className='flex flex-col w-full gap-[20px]'>
       <ContentsTitle title={title} />
-      <ContentsBody body={content} sliceBody={sliceBody} />
+      <ContentsBody body={content} />
       <div className='flex gap-[10px]'>
-        {tags.map((tag, index) => (
-          <div>{tag}</div>
+        {tags.map((tag) => (
+          <div
+            className={clsx(
+              'px-[5px] py-[2px] text-body rounded-[5px]',
+              tagColors[tag]
+            )}
+          >
+            #{tag}
+          </div>
         ))}
       </div>
     </div>

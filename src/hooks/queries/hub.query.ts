b62530b6f1807a5ef38fe6feeb12631d6a6c8b67
leechan <dlcks0601@gmail.com>
2025-01-26 00:04:03 +0900

    [Refactor] 쿼리 수정

--- src/hooks/queries/hub.query.ts
+++ src/hooks/queries/hub.query.ts
@@ -1,24 +1,57 @@
 // import { fetchHubs, HubsResponse } from '@/apis/hub.api';
 
-// import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import { fetchHub, fetchHubs, HubsResponse } from '@/apis/hub.api';
+import { roleItems } from '@/constants/hub/roleItems';
+import { roleTagItems } from '@/constants/hub/roleTagsItems';
+import {
+  InfiniteData,
+  useInfiniteQuery,
+  UseInfiniteQueryResult,
+  useQuery,
+  UseQueryResult,
+} from '@tanstack/react-query';
 
-// export const useInfiniteFetchHubs = (): UseQueryResult<HubsResponse, Error> => {
-//   return useQuery<HubsResponse>({
-//     queryKey: ['projects'],
-//     queryFn: () => fetchHubs(),
-//     retry: 10,
-//     staleTime: 60 * 1000,
-//     gcTime: 5 * 60 * 1000,
-//   });
-// };
+export const useInfiniteFetchHubs = (
+  skip: number,
+  limit: number,
+  role: keyof typeof roleItems,
+  unit: keyof typeof roleTagItems,
+  sort: string
+): UseInfiniteQueryResult<InfiniteData<HubsResponse>, Error> => {
+  return useInfiniteQuery<
+    HubsResponse,
+    Error,
+    InfiniteData<HubsResponse>,
+    [string, string, string]
+  >({
+    queryKey: ['projects', sort, role],
+    queryFn: ({ pageParam }) =>
+      fetchHubs({
+        skip: pageParam as number,
+        limit,
+        role,
+        unit,
+        sort,
+      }),
+    getNextPageParam: (lastPage) => {
+      if (!lastPage.page || lastPage.page.lastCursor === null) {
+        return undefined;
+      }
+      return lastPage.page.lastCursor;
+    },
+    initialPageParam: 0,
+  });
+};
 
 // // 피드 상세 불러오기
-// export const useFetchHub = (): UseQueryResult<HubResponse, Error> => {
-//   return useQuery<HubResponse>({
-//     queryKey: ['hub'],
-//     queryFn: () => fetchHub(),
-//     retry: 10,
-//     staleTime: 60 * 1000,
-//     gcTime: 5 * 60 * 1000,
-//   });
-// };
+export const useFetchHub = (
+  projectId: number
+): UseQueryResult<HubsResponse, Error> => {
+  return useQuery<HubsResponse>({
+    queryKey: ['project'],
+    queryFn: () => fetchHub(projectId),
+    retry: 10,
+    staleTime: 60 * 1000,
+    gcTime: 5 * 60 * 1000,
+  });
+};

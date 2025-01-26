ddf35f0b36ac2d5479ac31d649a00bdf6240d176
leechan <dlcks0601@gmail.com>
2025-01-26 15:12:15 +0900

    [Refactor] 목 데이터 주석처리 및 타입 수정

--- src/mocks/mock-data/hubItem.ts
+++ src/mocks/mock-data/hubItem.ts
@@ -1,4 +1,3 @@
-import { RoleProps } from '@/components/atoms/Role';
 import { hubTagItems } from '@/constants/hub/hubTagItems';
 import { meetingTagItems } from '@/constants/hub/meetingTagItems';
 import { roleItems } from '@/constants/hub/roleItems';
@@ -20,35 +19,11 @@ export interface HubItem {
   bookMarkCount: number;
   applyCount: number;
   viewCount: number;
+  createdAt: string;
   user: {
     userId?: number;
     profileUrl: string;
     nickname: string;
     role: string;
-    createdAt?: string;
   };
 }
-
-// export const hubItem: HubItemType[] = [
-//   {
-//     title:
-//       '실시간 여행 계획 플랫폼 프로젝트 진행합니다! 🔥 프론트엔드 개발자 많은 지원 부탁합니다.',
-//     role: 'PROGRAMMER' as const,
-//     startDate: '2025-01-06',
-//     duration: '6개월',
-//     workType: 'ONLINE',
-//     hubType: 'PROJECT',
-//     status: 'OPEN',
-//     detailRoles: ['HW/임베디드', 'SW/솔루션', 'IOS 개발자'],
-//     bookMarkCount: 12,
-//     applyCount: 5,
-//     viewCount: 6,
-//     thumbnailUrl: 'src/assets/images/contentimagetest.png',
-//     user: {
-//       profileUrl: '/src/assets/images/example.svg',
-//       nickname: 'leechan',
-//       role: 'Programmer',
-//       createdAt: '약 4시간 전',
-//     },
-//   },
-// ];
--- src/mocks/mock-data/hubSideBar.ts
+++ src/mocks/mock-data/hubSideBar.ts
@@ -1,28 +1,28 @@
-import { HubSideBarItemType } from '@/types/hubSideBarItem.type';
+// import { HubSideBarItemType } from '@/types/hubSideBarItem.type';
 
-export const HubSideBarItems: HubSideBarItemType[] = [
-  {
-    rank: 1,
-    userNickname: '아이유',
-    userProfileUrl: '/src/assets/images/example.svg',
-    userRole: 'Programmer',
-    hubTags: 'OUTSOURCING',
-    title: '심심해요.',
-  },
-  {
-    rank: 2,
-    userNickname: '홍길동',
-    userProfileUrl: '/src/assets/images/example.svg',
-    userRole: 'Designer',
-    hubTags: 'OUTSOURCING',
-    title: '디자인을 열심히 공부하고 있어요.',
-  },
-  {
-    rank: 3,
-    userNickname: '김코딩',
-    userProfileUrl: '/src/assets/images/example.svg',
-    userRole: 'Developer',
-    hubTags: 'PROJECT',
-    title: 'React를 공부 중입니다!',
-  },
-];
+// export const HubSideBarItems: HubSideBarItemType[] = [
+//   {
+//     rank: 1,
+//     userNickname: '아이유',
+//     userProfileUrl: '/src/assets/images/example.svg',
+//     userRole: 'Programmer',
+//     hubTags: 'OUTSOURCING',
+//     title: '심심해요.',
+//   },
+//   {
+//     rank: 2,
+//     userNickname: '홍길동',
+//     userProfileUrl: '/src/assets/images/example.svg',
+//     userRole: 'Designer',
+//     hubTags: 'OUTSOURCING',
+//     title: '디자인을 열심히 공부하고 있어요.',
+//   },
+//   {
+//     rank: 3,
+//     userNickname: '김코딩',
+//     userProfileUrl: '/src/assets/images/example.svg',
+//     userRole: 'Developer',
+//     hubTags: 'PROJECT',
+//     title: 'React를 공부 중입니다!',
+//   },
+// ];

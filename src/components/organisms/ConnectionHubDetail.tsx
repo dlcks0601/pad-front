72124a475a57073b95fa1d70f895ca4a0d9e4cb1
leechan <dlcks0601@gmail.com>
2025-01-27 05:00:40 +0900

    [Refactor] 데이터 리패치

--- src/components/organisms/ConnectionHubDetail.tsx
+++ src/components/organisms/ConnectionHubDetail.tsx
@@ -1,33 +1,39 @@
 import HubDetail from '@/components/molecules/contents/HubDetail';
 import { useFetchHub } from '@/hooks/queries/hub.query';
+import useAuthStore from '@/store/authStore';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const ConnectionHubDetail = () => {
-  const { projectId } = useParams<{ projectId: string }>(); // URL에서 projectId 가져오기
+  const { projectId } = useParams<{ projectId: string }>();
   const {
     data: ProjectData,
     isLoading: ProjectLoading,
     isError,
+    refetch,
   } = useFetchHub(Number(projectId));
 
-  console.log('ProjectData:', ProjectData);
+  useEffect(() => {
+    refetch();
+  }, [projectId, refetch]);
 
-  // project가 undefined일 수 있으므로 기본값 처리
+  const currentUserId = useAuthStore((state) => state.userInfo.userId);
+
+  console.log('ProjectData:', ProjectData);
+  console.log('Current User ID:', currentUserId);
+
   const project = ProjectData?.project;
 
-  // 로딩 중 처리
   if (ProjectLoading) {
     return <div>피드 로딩중...</div>;
   }
 
-  // 에러 또는 데이터 없음 처리
   if (isError || !project) {
     return <div>프로젝트 데이터를 불러오는 중 오류가 발생했습니다.</div>;
   }
 
   return (
     <div className='flex p-10px'>
-      {/* project가 존재하므로 안전하게 접근 */}
       <HubDetail
         title={project.title}
         hubType={project.hubType}
@@ -41,7 +47,7 @@ const ConnectionHubDetail = () => {
         content={project.content}
         createdAt={project.createdAt}
         manager={project.manager}
-        isOwnConnectionHub={project.manager.userId === Number(projectId)}
+        isOwnConnectionHub={project.manager.userId === currentUserId}
       />
     </div>
   );

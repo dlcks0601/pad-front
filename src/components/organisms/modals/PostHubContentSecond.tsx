0bcf423fcef6b21dba56bafec76cdba3a040c525
HANTAEDONG <htd0913@gmail.com>
2025-01-28 12:29:19 +0900

    [Refactor] 본문 작성하는 2번쨰 모달 구현

--- src/components/organisms/modals/PostHubContentSecond.tsx
+++ src/components/organisms/modals/PostHubContentSecond.tsx
@@ -1,36 +1,52 @@
-import TiptapEditor from '@/components/organisms/TiptapEditor';
+import { useEffect, useState } from 'react';
 import { HubState } from '@/store/postHubStore';
-import { useState } from 'react';
 import useHubStore from '@/store/postHubStore';
+import TiptapEditor from '@/components/organisms/TiptapEditor';
+import { usePostHub } from '@/hooks/queries/hub.query';
 
 interface PostHubContentSecondProps {
   onPrevious: () => void;
 }
 
 const PostHubContentSecond = ({ onPrevious }: PostHubContentSecondProps) => {
+  const {
+    content,
+    setContent,
+    title,
+    role,
+    hubType,
+    startDate,
+    duration,
+    workType,
+    skills,
+    detailRoles,
+  } = useHubStore();
+  const { mutate: postHub } = usePostHub();
+
   const [hubContent, setHubContent] = useState<Pick<HubState, 'content'>>({
     content: '',
   });
 
-  const { setContent } = useHubStore();
-
-  const handleChange = (field: keyof HubState, value: string | string[]) => {
-    setHubContent((prev) => ({
-      ...prev,
-      [field]: value,
-    }));
-  };
-
-  const isContentEmpty = (html: string) => {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html');
-    const textContent = doc.body.textContent?.trim() || '';
-    return textContent === '';
-  };
+  useEffect(() => {
+    setHubContent({ content });
+  }, [content]);
 
   const handleSaveContent = () => {
     setContent(hubContent.content);
     console.log('Content 저장됨:', hubContent.content);
+
+    postHub({
+      title,
+      content: hubContent.content,
+      role,
+      hub_type: hubType,
+      start_date: startDate,
+      duration,
+      work_type: workType,
+      recruiting: false, // Default or dynamic value
+      skills,
+      detail_roles: detailRoles,
+    });
   };
 
   return (
@@ -39,36 +55,25 @@ const PostHubContentSecond = ({ onPrevious }: PostHubContentSecondProps) => {
       <div className='flex flex-col w-full px-[10px] py-[20px] h-[573px] overflow-y-scroll scrollbar-hide border rounded-md border-black'>
         <TiptapEditor
           content={hubContent.content}
-          setContent={(value) => handleChange('content', value)}
+          setContent={(value) => setHubContent({ content: value })}
         />
       </div>
       <div className='flex w-full justify-end'>
-        <div className='flex gap-[10px]'>
-          <button
-            className='w-[50px] h-[35px] text-[14px] bg-[#FF5E5E] text-white rounded'
-            onClick={onPrevious}
-          >
-            이전
-          </button>
-          <button
-            className='w-[50px] h-[35px] text-[14px] bg-[#00C859] text-white rounded'
-            disabled={isContentEmpty(hubContent.content)}
-            onClick={handleSaveContent}
-          >
-            완료
-          </button>
-        </div>
+        <button
+          className='w-[50px] h-[35px] text-[14px] bg-[#FF5E5E] text-white rounded'
+          onClick={onPrevious}
+        >
+          이전
+        </button>
+        <button
+          className='w-[50px] h-[35px] text-[14px] bg-[#00C859] text-white rounded'
+          onClick={handleSaveContent}
+        >
+          완료
+        </button>
       </div>
     </div>
   );
 };
 
 export default PostHubContentSecond;
-
-{
-  /* {errors.content && (
-          <p className='flex flex-col text-red-600 text-[14px] mt-5 absolute'>
-            내용을 작성해주세요.
-          </p>
-        )} */
-}

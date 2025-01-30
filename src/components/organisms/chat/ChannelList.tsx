93c7950c2238e0515ddfdf97d0edf0f5af38ef29
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-01-30 09:34:27 +0900

    [Merge] feature/82-chatting-exit -> develop
    * [Refactor] 드롭다운 컴포넌트에서 인덱스 대신 id 사용하도록 수정
    
    * [Feat] 채팅방 나가기 기능 구현
    
    * [Style] 나가기 버튼 UI 보완
    
    * [Style] 나가기 버튼 UI 보완

--- src/components/organisms/chat/ChannelList.tsx
+++ src/components/organisms/chat/ChannelList.tsx
@@ -1,4 +1,5 @@
 import Avatar from '@/components/atoms/Avatar';
+import Icon from '@/components/atoms/Icon';
 import Title from '@/components/atoms/Title';
 import { ListItem } from '@/components/molecules/ListItem';
 import useAuthStore from '@/store/authStore';
@@ -6,6 +7,7 @@ import { ChatState, useChatStore } from '@/store/chatStore';
 import { Channel } from '@/types/channel.type';
 import { formatDateFromNow } from '@/utils/format';
 import clsx from 'clsx';
+import { MouseEvent as ReactMouseEvent } from 'react';
 import { useShallow } from 'zustand/shallow';
 
 interface ChannelListProps {
@@ -13,10 +15,11 @@ interface ChannelListProps {
 }
 
 const ChannelList = ({ channels }: ChannelListProps) => {
-  const { joinChannel, currentChannelId } = useChatStore(
+  const { joinChannel, currentChannelId, exitChannel } = useChatStore(
     useShallow((state) => ({
       joinChannel: state.joinChannel,
       currentChannelId: state.currentChannelId,
+      exitChannel: state.exitChannel,
     }))
   );
   const user = useAuthStore((state) => state.userInfo);
@@ -25,15 +28,39 @@ const ChannelList = ({ channels }: ChannelListProps) => {
     if (channelId === currentChannelId) return;
     joinChannel(user!.userId, channelId);
   };
+
+  const handleChannelClick = (
+    e: ReactMouseEvent,
+    channelId: Channel['channelId']
+  ) => {
+    if (e.button === 0) {
+      switchChannel(channelId);
+    }
+  };
+
+  const handleChannelExit = () => {
+    if (window.confirm('현재 채팅방을 나가시겠습니까?')) {
+      // console.log(`${currentChannelId} 번 채팅방 나감`);
+      const userId = useAuthStore.getState().userInfo.userId;
+      exitChannel(userId, currentChannelId!);
+    } else {
+      console.log('나가기 취소');
+    }
+  };
+
   return (
-    <ul className='grow flex flex-col gap-[24px]'>
-      {Object.entries(channels).map(([channelId, channel]) => {
+    <ul className='grow flex flex-col gap-[24px] pb-[50px]'>
+      {Object.entries(channels).map(([_, channel]) => {
         return (
-          <li key={channelId} onClick={() => switchChannel(+channelId)}>
+          <li
+            key={channel.channelId}
+            onClick={(e) => handleChannelClick(e, channel.channelId)}
+            className='group relative'
+          >
             <ListItem
               className={clsx([
                 'h-[62px] rounded-[8px] cursor-pointer items-center p-[10px] gap-[10px]',
-                +channelId === currentChannelId
+                channel.channelId === currentChannelId
                   ? 'bg-[#EDECF3]'
                   : 'hover:bg-[#EDECF3] ',
               ])}
@@ -62,6 +89,18 @@ const ChannelList = ({ channels }: ChannelListProps) => {
           </li>
         );
       })}
+      {currentChannelId && (
+        <div className='mt-auto flex justify-end'>
+          <button
+            onClick={handleChannelExit}
+            aria-label='채팅방 나가기'
+            className='flex items-center text-darkgray hover:text-[#333] h-[38px] gap-[5px]'
+          >
+            <div>나가기</div>
+            <Icon type='exit' className='text-inherit w-[24px] h-[24px]' />
+          </button>
+        </div>
+      )}
     </ul>
   );
 };

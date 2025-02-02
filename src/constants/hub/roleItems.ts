1b9521b6938a922fae0a7d2050badfb7fdf9ae8d
leechan <dlcks0601@gmail.com>
2025-02-02 21:53:40 +0900

    [Refactor] 롤 아이템 수정

--- src/constants/hub/roleItems.ts
+++ src/constants/hub/roleItems.ts
@@ -3,5 +3,12 @@ export const roleItems = {
   Artist: 'ARTIST',
   Designer: 'DESIGNER',
 } as const;
+
 export type roleItemsKey = keyof typeof roleItems;
-export type roleItemsValue = (typeof roleItems)[keyof typeof roleItems];
+export type roleItemsValue = (typeof roleItems)[roleItemsKey];
+
+export const roleValueToKeyMap: Record<roleItemsValue, roleItemsKey> = {
+  PROGRAMMER: 'Programmer',
+  ARTIST: 'Artist',
+  DESIGNER: 'Designer',
+};

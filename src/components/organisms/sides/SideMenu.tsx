86d9ed5c0aede295fc0f600d1c7400a839d3c4a0
HANTAEDONG <htd0913@gmail.com>
2025-02-02 21:50:43 +0900

    [Refactor] 알림창 createPortal로 구현하고 외부 클릭시 모달 닫히도록 수정

--- src/components/organisms/sides/SideMenu.tsx
+++ src/components/organisms/sides/SideMenu.tsx
@@ -13,6 +13,7 @@ import {
   useFetchMissedNotifications,
   usePatchNotificationAsRead,
 } from '@/hooks/queries/notification.query';
+import { createPortal } from 'react-dom';
 
 interface NotificationProp {
   notificationId: number;
@@ -123,6 +124,25 @@ const SideMenu = () => {
     },
   ];
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        notificationRef.current &&
+        !notificationRef.current.contains(event.target as Node)
+      ) {
+        setShowNotificationBox(false);
+      }
+    };
+
+    if (showNotificationBox) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [showNotificationBox]);
+
   return (
     <>
       {isSearchModalOpen && <SearchModal onClose={closeSearchModal} />}
@@ -132,35 +152,39 @@ const SideMenu = () => {
           <Logo />
         </div>
         <Menu items={menuItems} />
-        {showNotificationBox && (
-          <div
-            ref={notificationRef}
-            className='absolute left-[90px] top-[50px] w-[370px] h-[700px] bg-white bg-opacity-95 rounded-xl drop-shadow-lg px-[20px] py-[20px] overflow-y-auto z-50'
-          >
-            <div className='flex w-full flex-col items-center gap-[10px]'>
-              <div className='text-[18px] font-semibold text-[#48484a]'>
-                알림 📫
-              </div>
-              {messages.length === 0 ? (
-                <div className='text-[16px] text-[#828282]'>
-                  현재 새로운 알림이 없습니다.
-                </div>
-              ) : (
-                <div className='flex w-full flex-col gap-[20px]'>
-                  {messages.map((message, index) => (
-                    <div
-                      key={index}
-                      className='flex w-full justify-start text-[14px] items-center gap-[10px]'
-                    >
-                      <Avatar src={message.senderProfileUrl} size={'xs'} />
-                      <div>{message.message}</div>
+        {showNotificationBox &&
+          createPortal(
+            <div className='fixed inset-0 flex items-center justify-center z-[1000]'>
+              <div
+                ref={notificationRef}
+                className='absolute left-[90px] top-[50px] w-[370px] h-[700px] bg-white bg-opacity-95 rounded-xl drop-shadow-lg px-[20px] py-[20px] overflow-y-auto z-50'
+              >
+                <div className='flex w-full flex-col items-center gap-[10px]'>
+                  <div className='text-[18px] font-semibold text-[#48484a]'>
+                    알림 📫
+                  </div>
+                  {messages.length === 0 ? (
+                    <div className='text-[16px] text-[#828282]'>
+                      현재 새로운 알림이 없습니다.
                     </div>
-                  ))}
+                  ) : (
+                    <div className='flex w-full flex-col gap-[20px]'>
+                      {messages.map((message, index) => (
+                        <div
+                          key={index}
+                          className='flex w-full justify-start text-[14px] items-center gap-[10px]'
+                        >
+                          <Avatar src={message.senderProfileUrl} size={'xs'} />
+                          <div>{message.message}</div>
+                        </div>
+                      ))}
+                    </div>
+                  )}
                 </div>
-              )}
-            </div>
-          </div>
-        )}
+              </div>
+            </div>,
+            document.body
+          )}
 
         <div className='relative' ref={loginRef}>
           <Avatar

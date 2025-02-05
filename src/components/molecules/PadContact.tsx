33059df2083b841aa65c088479122b7fe0262b07
JaeHye0k <55015406+JaeHye0k@users.noreply.github.com>
2025-02-05 21:36:05 +0900

    [Deploy] develop -> main
    * [Fix] SSE 연결 안되는 문제 해결
    
    * [Fix] 알림창 열였을 때 key 중복 에러 뜨는 문제 해결
    
    * [Fix] 알림 중복 문제 해결
    
    * [Remove] 필요없는 컴포넌트 삭제
    
    * [Refactor] 패드 contact 수정
    
    ---------
    
    Co-authored-by: HANTAEDONG <htd0913@gmail.com>
    Co-authored-by: leechan <dlcks0601@gmail.com>

--- src/components/molecules/PadContact.tsx
+++ src/components/molecules/PadContact.tsx
@@ -1,11 +1,94 @@
+import GitHubIcon from '@/assets/icons/github.svg';
+
 const PadContact = () => {
   return (
-    <div className='px-[30px] py-3 bg-white rounded-[10px] text-gray-600 text-sm leading-relaxed space-y-1'>
-      <p className='font-bold'>PAD inc.</p>
-      <p>Seoul, Republic of Korea</p>
-      <p>02 1234 5678</p>
-      <p>info@PAD.com</p>
-      <p>2024.12.31 | Title : PAD</p>
+    <div className='flex flex-col gap-[5px] px-[30px] py-3 bg-white rounded-[10px] text-gray-600 leading-relaxed space-y-1'>
+      <div className='flex w-full justify-between items-center'>
+        <p className='font-bold text-[24px]'>PAD inc.</p>
+      </div>
+      <div className='flex flex-col font-semibold text-[16px]'>Team PAD</div>
+      <div className='flex w-full justify-between'>
+        <div className='flex flex-col gap-[5px]'>
+          <div className='flex font-semibold text-[20px] '>FE</div>
+          <div className='flex items-center gap-[5px] w-full justify-between'>
+            <div>chaeyun</div>
+            <a href='https://github.com/chaeyun-sim'>
+              <img
+                src={GitHubIcon}
+                alt='GitHub'
+                className='w-5 h-5 hover:opacity-80 transition-opacity'
+              />
+            </a>
+          </div>
+
+          <div className='flex items-center gap-[5px] w-full justify-between'>
+            <div>jaehyuk</div>
+            <a href='https://github.com/JaeHye0k'>
+              <img
+                src={GitHubIcon}
+                alt='GitHub'
+                className='w-5 h-5 hover:opacity-80 transition-opacity'
+              />
+            </a>
+          </div>
+          <div className='flex items-center gap-[5px] w-full justify-between'>
+            <div>chan</div>
+            <a href='https://github.com/dlcks0601'>
+              <img
+                src={GitHubIcon}
+                alt='GitHub'
+                className='w-5 h-5 hover:opacity-80 transition-opacity'
+              />
+            </a>
+          </div>
+          <div className='flex items-center gap-[5px] w-full justify-between'>
+            <div>taedong</div>
+            <a href='https://github.com/HANTAEDONG'>
+              <img
+                src={GitHubIcon}
+                alt='GitHub'
+                className='w-5 h-5 hover:opacity-80 transition-opacity'
+              />
+            </a>
+          </div>
+        </div>
+        <div className='flex flex-col gap-[5px]'>
+          <div className='flex font-semibold text-[20px]'>BE</div>
+          <div className='flex items-center gap-[5px] w-full justify-between'>
+            <div>seongmin</div>
+            <a href='https://github.com/Ss0Mae'>
+              <img
+                src={GitHubIcon}
+                alt='GitHub'
+                className='w-5 h-5 hover:opacity-80 transition-opacity'
+              />
+            </a>
+          </div>
+          <div className='flex items-center gap-[5px] w-full justify-between'>
+            <div>donghyun</div>
+            <a href='https://github.com/peppertown'>
+              <img
+                src={GitHubIcon}
+                alt='GitHub'
+                className='w-5 h-5 hover:opacity-80 transition-opacity'
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className='flex font-light text-[12px] cursor-pointer'>
+        info@PAD.com
+      </div>
+      <div className='flex font-thin text-[10px] w-full justify-between items-center'>
+        © 2024 PAD. All Rights Reserved.
+        <a href='https://github.com/NoGiveUpWeCarry'>
+          <img
+            src={GitHubIcon}
+            alt='GitHub'
+            className='w-5 h-5 hover:opacity-80 transition-opacity'
+          />
+        </a>
+      </div>
     </div>
   );
 };

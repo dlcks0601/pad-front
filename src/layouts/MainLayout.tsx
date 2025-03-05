5eb2679338150c2ca3270ce9288b7960b969d9b5
한태동 <hantaedong@hantaedong-ui-MacBookAir.local>
2025-03-05 15:19:20 +0900

    [Style] 모바일 사이즈 레이아웃 구현

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import SideMenu from '@/components/organisms/sides/SideMenu';
 import MainSideBar from '@/components/organisms/sides/MainSideBar';
+import MobileNav from '@/components/organisms/sides/MobileNav';
 
 const MainLayout = () => {
   const preventInfo = ['/login'];
@@ -8,8 +9,13 @@ const MainLayout = () => {
 
   return (
     <div className='min-h-screen flex flex-col lg:flex-row lg:px-[10px]'>
-      <div className='sticky top-0 w-full lg:w-[68px] h-[56px] lg:h-screen lg:px-4 flex items-center justify-between bg-blue-300'>
-        <SideMenu />
+      <div className='sticky top-0 w-full lg:w-[68px] h-[52px] lg:h-screen lg:px-4 flex items-center justify-between'>
+        <div className='hidden lg:block w-full h-full'>
+          <SideMenu />
+        </div>
+        <div className='block lg:hidden w-full h-full'>
+          <MobileNav />
+        </div>
       </div>
       <div className='flex-1 overflow-y-auto'>
         <div className='max-w-[800px] w-full mx-auto py-6'>

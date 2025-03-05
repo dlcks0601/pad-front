6a1139fb518122359474d3232293f2578c722e86
한태동 <hantaedong@hantaedong-ui-MacBookAir.local>
2025-03-05 15:42:21 +0900

    [Style] 모바일 레이아웃 hub에 적용

--- src/layouts/HubLayout.tsx
+++ src/layouts/HubLayout.tsx
@@ -1,15 +1,21 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import SideMenu from '@/components/organisms/sides/SideMenu';
 import HubSideBar from '@/components/organisms/sides/HubSideBar';
+import MobileNav from '@/components/organisms/sides/MobileNav';
 
 const HubLayout = () => {
   const preventInfo = ['/login', '/settings'];
   const location = useLocation();
 
   return (
-    <div className='min-h-screen flex px-[10px]'>
-      <div className='sticky top-0 h-screen w-[68px] z-10'>
-        <SideMenu />
+    <div className='min-h-screen flex flex-col lg:flex-row lg:px-[10px]'>
+      <div className='sticky top-0 w-full lg:w-[68px] h-[52px] lg:h-screen lg:px-4 flex items-center justify-between bg-white z-50'>
+        <div className='hidden lg:block w-full h-full'>
+          <SideMenu />
+        </div>
+        <div className='block lg:hidden w-full h-full'>
+          <MobileNav />
+        </div>
       </div>
 
       <div className='flex-1 overflow-y-auto'>
